00284389ec7b2fbb6ec2d2dce4cbd5618ce96623
JEJodesty <joshua.jodesty@gmail.com>
2022-05-02 12:56:29 -0400

    catNode: added apps to sys path

--- apps/cat0/execute.py
+++ apps/cat0/execute.py
@@ -1,11 +1,12 @@
 import sys
-
 sys.path.append("../..")
-from apps.cat0 import catFactory
-
-# catFactory.build_cats()
-catFactory.terraform()
-catFactory.execute()
+print(sys.path)
+exit()
+# from apps.cat0 import catFactory
+#
+# # catFactory.build_cats()
+# catFactory.terraform()
+# catFactory.execute()
 
 # 15:35:43
 # 15:37:28
