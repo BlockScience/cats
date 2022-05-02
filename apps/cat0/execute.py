feb1dd8293f43b40869c3298391c9a43852e04d8
JEJodesty <joshua.jodesty@gmail.com>
2022-05-02 14:55:17 -0400

    catNode: add py deps

--- apps/cat0/execute.py
+++ apps/cat0/execute.py
@@ -1,14 +1,12 @@
-# import sys
-# from pathlib import Path
-# apps_dir = str(Path(__file__).resolve().parents[1])
-# sys.path.append(apps_dir)
-# print(sys.path)
-# exit()
-from apps.cat0 import catFactory
+import os
 
-# catFactory.build_cats()
-catFactory.terraform()
-catFactory.execute()
+print(os.getenv('SPARK_HOME'))
+
+# from apps.cat0 import catFactory
+#
+# # catFactory.build_cats()
+# catFactory.terraform()
+# catFactory.execute()
 
 # 15:35:43
 # 15:37:28
