691eb0b714f33b7ea412c0f69ab163bbdccc6b52
JEJodesty <joshua.jodesty@gmail.com>
2024-01-29 15:43:33 -0500

    update insetallation guide pt1

--- cats/__init__.py
+++ cats/__init__.py
@@ -2,10 +2,6 @@ import os
 from os.path import dirname, abspath
 CATS_HOME = dirname(dirname(abspath(__file__)))
 DATA_HOME = CATS_HOME + '/data'
-# SPARK_HOME = os.getenv('SPARK_HOME')
-# DRIVER_IPFS_DIR = f'{CATS_HOME}/catStore'
-# CATSTORE = f'{CATS_HOME}/catStore/cats-public/cad-store'
-# ToDo: change w3 access
 CWD = os.getcwd()
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
